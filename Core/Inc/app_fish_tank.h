a2cdc1dbc764da67b26a0d0382395a6ed20e8d00
waweroit <waweroit@gmail.com>
2026-05-08 13:10:49 +0200

    Add fish tank application interface

---
+++ Core/Inc/app_fish_tank.h
@@ -0,0 +1,15 @@
+#ifndef INC_APP_FISH_TANK_H_
+#define INC_APP_FISH_TANK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void App_Init(void);
+void App_Process(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_APP_FISH_TANK_H_ */
